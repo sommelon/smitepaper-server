27e77b20fd55b89f7d5e338122f665ec071eb733
Simeon Králik <simeon.kralik@gmail.com>
2022-04-18 21:06:29 +0200

    feat: Create basic entities and CRUD API.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into wallpaper (id, name, image_link, image_width, image_height) values (2, 'athena', 'http://link.com', 1920, 1080);
+insert into wallpaper (id, name, image_link, image_width, image_height) values (1, 'loki', 'http://link.com', 1920, 1080);
+insert into tag (id, title) values (1, 'greek');
+insert into tag (id, title) values (2, 'roman');
+insert into wallpaper_tag values (1, 1);
+insert into wallpaper_tag values (2, 2);
